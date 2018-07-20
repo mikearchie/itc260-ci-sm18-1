234d2d66c5261709e75fb288d174fb3a3a12421e
Mike Archie <rusty505@yahoo.com>
2018-07-20 00:15:50 -0700

    Initial commit of news views

---
+++ application/views/news/index.php
@@ -0,0 +1,24 @@
+<?php
+//application/view/news/index.php
+
+$this->load->view($this->config->item('theme') . 'header');
+
+
+?>
+
+<h2><?php echo $title; ?></h2>
+<div><?=anchor('news/create','Create News Item')?></div>
+
+<?php foreach ($news as $news_item): ?>
+
+        <h3><?php echo $news_item['title']; ?></h3>
+        <div class="main">
+                <?php echo $news_item['text']; ?>
+        </div>
+        <p><a href="<?php echo site_url('news/'.$news_item['slug']); ?>">View article</a></p>
+
+<?php endforeach;
+
+$this->load->view($this->config->item('theme') . 'footer');
+
+?>
---
+++ application/views/news/success.php
@@ -0,0 +1,7 @@
+<?php
+//application/views/news/success.?>
+
+<p>Yay! You entered data!</p>
+<p>Wouldn't it be great, if we showed it to you?!</p>
+<p><a href="<?php echo site_url('news/') ?>">View News</a></p>
+<p><a href="<?php echo site_url('news/create/') ?>">Create another</a></p>
